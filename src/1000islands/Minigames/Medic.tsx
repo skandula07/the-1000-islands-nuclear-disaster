680c9710dde4cc9a9196060d6d1d95e54616bf4d
sree :) <kandula.s@northeastern.edu>
2024-12-03 18:15:59 -0500

    medical symptoms

--- src/1000islands/Minigames/Medic.tsx
+++ src/1000islands/Minigames/Medic.tsx
@@ -7,13 +7,43 @@ export default function Medic() {
 
 
     const poisoning = [
+        {"ailment" : "", "color": "", "desc" : ""},
         {"ailment" : "Leukemia", "color": "bg-red-600", "desc" : "Leukemia is a cancer of the blood. Ionizing radiation can be the cause of almost all types of leukemias"},
         {"ailment" : "Immunocompromised", "color": "bg-blue-600", "desc" : "Radiation can completely ruin a person's immune system when dosed in large amounts. This makes you highly suceptible to all sorts of viruses and infections waiting to strike."},
-        {"ailment" : "Nausea", "color": "bg-violet-600", "desc" : ""},
-        {"ailment" : "Skin Burns", "color": "bg-orange-600", "desc" : "Radiation can completely ruin a person's immune system when dosed in large amounts. This makes you highly suceptible to all sorts of viruses and infections waiting to strike."},
+        {"ailment" : "Nausea", "color": "bg-violet-600", "desc" : "One of the first side effects of radiation poisoning can seem pretty typical, nausea. You may feel the world spinning and your stomach churning, but it's really the uranium ruining your body."},
+        {"ailment" : "Skin Burns", "color": "bg-orange-600", "desc" : "One of the scariest and most painful symptoms is the gruesome skin burns and boils. It looks and feels almost like your skin is falling off."},
     ]
 
-    const [ailment, setAilment] = useState();
+    const [ailment, setAilment] = useState("");
+
+    function handleAilment(a) {
+        return (
+            setAilment(a)
+        );
+    }
+    
+    const [desc, setDesc] = useState("");
+
+    function handleDesc(a) {
+        return (
+            setDesc(a)
+        );
+    }
+    const [color, setColor] = useState("");
+
+    function handlColor(a) {
+        return (
+            setColor(a)
+        );
+    }
+
+    function handleClick(a, c, d) {
+        handlColor(c);
+        handleAilment(a);
+        handleDesc(d);
+
+
+    }
 
 
 
@@ -28,6 +58,8 @@ export default function Medic() {
 
             <div className="flex">
             <img src="/images/soutline.png" alt="operation" width="50%" className="align-self-center" />
+            
+            <div>
             <p className="text-xs">
             The amount of time between exposure and when these symptoms develop is a clue to how much radiation a person has absorbed.
 
@@ -36,29 +68,35 @@ After the first round of symptoms, a person with radiation sickness may have a b
 If you've had a mild exposure, it may take hours to weeks before symptoms begin. But with high exposure, symptoms can begin minutes to days after exposure.
             </p>
             
-            </div>
+            
 
             <div>
 
+                <div className="flex m-3">
                 {poisoning.map((p) => 
                 <div>
-                   <button className={`${p.color} hover:text-white text-black font-bold py-2 px-4 rounded-full duration-100`}
-                   onClick={() => setAilment(p)}
+
+                   <button id={p.ailment} className={`${p.color} hover:text-white text-black font-bold py-2 px-4 rounded-full duration-100 m-2`}
+                   onClick={() => handleClick(p.ailment, p.color, p.desc) }
                    >
-                    {/* {p.ailment} */}
-                    ?
+                    {/* <label htmlFor={p.ailment} className="p-1 text-white"> {p.ailment} </label> */}
                 </button>  
                 </div>
                 )}
-                {ailment &&
+                </div>
                 <div>
 
-                    <div className={`${ailment.ailment}`}></div>
+                    <div className={`rounded ${color} p-9 m-3`}>
+                        <h1 className="text-3xl underline text-white"> {ailment} </h1>
+                        <p className="text-md"> {desc} </p>
+                    </div>
 
                 </div>
-                }
+                </div>
+              
                
 
+            </div>
             </div>
             
 
